02978d070bf289c0df224e53715dbc5a23a75db7
tuyenkim-2004 <kimtuyen10072004@gmail.com>
2024-11-18 22:58:14 +0700

    create brand develop

---
+++ Views/Pages/Home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <img src="/ProjectWebPhp/Assets/images/banner_1.png" alt="">
+    </section>
+</body>
+</html>
